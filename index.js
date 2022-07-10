abe5c99417268c0f9ed72c2c82c0f13d1cb5e4d3
loetitia <audiger.loetitia@gmail.com>
2022-07-10 15:31:19 +0200

    add post route event

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ app.get('/events/:id', (req, res) => {
     res.send(event)
 })
 
+//Create event
+app.post('/events', (req, res) => {
+    res.send('Create event')
+})
+
 app.listen(8000, () => {
     console.log('Running app express on port 8000')
 })
